d76515fabd0eaa46a09dfa1f22387d0c6fd2e1dc
Reuel Igbokwe <reueligbokwe90@gmail.com>
2026-08-30 00:28:23 +0100

    Created Space class

---
+++ Space.py
@@ -0,0 +1,8 @@
+class Space:
+    def __init__(self,x,y,data,width,height):
+        self.children = []
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
