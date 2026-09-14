061cfbd02078db9e2639930cac2485648d59b7e7
Reuel <reueligbokwe90@gmail.com>
2026-09-14 20:38:31 +0100

    fixed recursive split functionality to properly split height wise or width wise on a coin flip

--- Generator.py
+++ Generator.py
@@ -14,32 +14,51 @@ def recursive_split(node):
         node (object): This takes a root node and recusrsively divides creating a full
         tree
     """
-    if node.width < MIN_WIDTH * 2 and node.height < MIN_HEIGHT * 2:
+    if node.width < MIN_WIDTH * 2 or node.height < MIN_HEIGHT * 2:
         return
         
     ratio = random.uniform(0.4,0.6)
+    coin = random.random()
     
-    #left space splitting
-    left_x = node.x
-    left_y = node.y
-    left_width = node.width * ratio
-    left_height = node.height
-    
-    #create the space objects
-    left_zone = Space(left_x,left_y,left_width,left_height)
-    node.left = left_zone
-    
-    #right space splitting
-    right_x = node.x + left_width
-    right_y = node.y
-    right_width = node.width - left_width
-    right_height = node.height
-    
-    right_zone = Space(right_x,right_y,right_width,right_height)
-    node.right = right_zone
+    if coin < 0.5:
+        #width splitting
+        left_x = node.x
+        left_y = node.y
+        left_width = node.width * ratio
+        left_height = node.height 
+        
+        right_x = node.x + left_width
+        right_y = node.y
+        right_width = node.width - left_width
+        right_height = node.height
+        
+        left_zone = Space(left_x,left_y,left_width,left_height)
+        node.left = left_zone 
+        right_zone = Space(right_x,right_y,right_width,right_height)
+        node.right = right_zone
+        
+    else:
+        # height splitting
+        left_x = node.x
+        left_y = node.y
+        left_width = node.width 
+        left_height = node.height * ratio
+                
+        
+        right_x = node.x 
+        right_y = node.y + left_height
+        right_width = node.width
+        right_height = node.height - left_height
+        
+        left_zone = Space(left_x,left_y,left_width,left_height)
+        node.left = left_zone 
+        right_zone = Space(right_x,right_y,right_width,right_height)
+        node.right = right_zone
+        
     
     recursive_split(left_zone)
     recursive_split(right_zone)
+
    
 def collect_rooms(node, rooms=None):
     if rooms is None:
@@ -53,3 +72,17 @@ def collect_rooms(node, rooms=None):
         collect_rooms(node.left, rooms)
         collect_rooms(node.right, rooms)
         return rooms
+    
+    
+root = Space(0,0,2000,2000)
+
+recursive_split(root)
+rooms = collect_rooms(root)
+for room in rooms:
+    print(
+        "x:", room.x,
+        "y:", room.y,
+        "width:", room.width,
+        "height:", room.height
+    )
+
